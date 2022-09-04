eebbb63b4aaaa5b8bf538f651914bd12c20ab4c1
Andy <andy@woodlair.com>
2022-09-03 21:59:35 -0500

    Updated review card to conditionally show link button, and optionally recieve classname prop

--- src/components/ReviewCard/index.tsx
+++ src/components/ReviewCard/index.tsx
@@ -1,10 +1,11 @@
 import { FunctionComponent } from 'react';
+import { Link } from 'react-router-dom';
 import { convertRating } from './utils';
 import { format } from 'date-fns';
 import commentIcon from '../../resources/commentIcon.svg';
 import './styles.scss';
 
-interface ReviewCardPropsModel {
+export interface ReviewCardPropsModel {
   id: string;
   author: string;
   place: string;
@@ -12,6 +13,7 @@ interface ReviewCardPropsModel {
   rating: number;
   content: string;
   detailsView?: boolean;
+  className?: string;
 }
 
 export const ReviewCard: FunctionComponent<ReviewCardPropsModel> = ({
@@ -21,11 +23,11 @@ export const ReviewCard: FunctionComponent<ReviewCardPropsModel> = ({
   published_at,
   rating,
   content,
-  detailsView
+  detailsView,
+  className
 }: ReviewCardPropsModel) => {
-
   return (
-    <div className='review-card'>
+    <div className={`review-card ${className}`}>
       <div className='card-padding'>
         <div className='place'>{place}</div>
         {convertRating(rating)}
@@ -35,9 +37,7 @@ export const ReviewCard: FunctionComponent<ReviewCardPropsModel> = ({
           <div className='author'>{author}</div>
           <div className='published-date'>{format(new Date(published_at), 'MM/dd/yyyy')}</div>
           <button className='comment-icon'>
-            {/* TODO: Add on click and go to new view */}
-            {/* If  */}
-            {!detailsView && <img src={commentIcon} alt='comment-button' /> } 
+            {!detailsView && <Link to={`/reviews/reviewId=${id}`}><img src={commentIcon} alt='comment-button' /></Link> } 
           </button>
         </div>
       </div>
